2df9fabe3b8ff7d06aedda6a2fcfcd875bd81025
buptgn <30334550+buptgn@users.noreply.github.com>
2017-08-08 10:07:43 +0900

    Add files via upload

---
+++ ArrangeListItem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+//using System.IO;
+//using ISC.EC.Download.WF.Common;
+//using ISC.EC.Download.Entities;
+
+namespace ISC.EC.Download.WF.AT
+{
+    public sealed class ArrangeListItem : CodeActivity
+    {
+        public InOutArgument<List<string>> List { set; get; }
+
+        public OutArgument<bool> HasChanged { get; set; }
+        //public InArgument<DownloadItem> inDownloadItem { set; get; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            List<string> list = context.GetValue(List);
+            //DownloadItem di = context.GetValue(inDownloadItem);
+            bool flag = false;
+            int index = 0;
+            
+            foreach(var row in list)
+            {
+                if (!row.Contains(","))
+                {
+                    index++;
+                }
+                else break;
+            }
+
+            /*/-----------------------------------------
+             
+              
+            if(list[4].Contains("ERROR"))
+            {
+                string str = string.Empty;
+                foreach (var row in context.GetValue(List))
+                {
+                    
+                    if (!row.Equals(""))
+                    {
+                        str += row;
+                        str += ",";
+                    }
+                    
+                }
+                str += string.Format("itemID={0}, promID={1}", di.Id, di.PromotionId);
+                WriteLogClass.WriteLog(str);
+                
+            }
+            //-----------------------------------------*/
+
+            if (index > 0)
+            {
+                list.RemoveRange(0, index);
+                flag = true;
+            }
+            
+
+            context.SetValue(List, list);
+            context.SetValue(HasChanged, flag);
+        }
+    }
+}
---
+++ SelectListItems.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using System.IO;
+using ISC.EC.Download.WF.Common;
+using ISC.EC.Download.Entities;
+
+namespace ISC.EC.Download.WF.AT
+{
+    public sealed class SelectListItems : CodeActivity
+    {
+        public InArgument<List<string>> inList { set; get; }
+        public InArgument<string> inStateTypeID { set; get; }
+        //public InArgument<DownloadItem> inDownloadItem { set; get; }
+        public OutArgument<List<string>> outList { set; get; }
+        
+        protected override void Execute(CodeActivityContext context)
+        {
+            List<string> oldlist = context.GetValue(inList);
+            string[] stateArr = context.GetValue(inStateTypeID).Split(',');
+            List<string> newlist = new List<string>();
+
+            //DownloadItem di = context.GetValue(inDownloadItem);
+            if (oldlist.Count > 0)
+            {
+                newlist.Add(oldlist[0]);
+
+                if (oldlist[0].Split(',').Length != 24)
+                {
+                    //string strLog = string.Format("标题长度不一致！ PromID={0}, ItemId={1},Title_Length={2}", di.PromotionId, di.Id ,oldlist[0].Split(',').Length);
+                    //WriteLogClass.WriteLog(strLog);
+                }
+
+                if (stateArr != null)
+                {
+                    foreach (var row in oldlist)
+                    {
+                        bool flag = false;
+                        foreach (var statevalue in stateArr)
+                        {
+                            if (statevalue != "")
+                            {
+                                if (row.Split(',')[0].Contains(statevalue) && !flag)
+                                {
+                                    newlist.Add(row);
+                                    flag = true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //WriteLogClass.WriteLog(string.Format("Key与Prom不匹配，Prom_ID={0}",di.PromotionId));
+            }
+
+            
+            context.SetValue(outList, newlist);
+        }
+
+    }
+}
